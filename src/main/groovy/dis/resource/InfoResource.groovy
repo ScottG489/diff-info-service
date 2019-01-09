3aea3bfdfe9b541355b47c923077696ddd597779
Scott Giminiani <scottg489@gmail.com>
2019-01-09 09:48:05 -0800

    Set Access-Control-Allow-Origin to '*'
    This is meant to be an open endpoint and usable from any origin.
    
    - Require POST method on endpoint instead of GET

--- src/main/groovy/dis/resource/InfoResource.groovy
+++ src/main/groovy/dis/resource/InfoResource.groovy
@@ -8,26 +8,29 @@ import udp.parse.UnifiedDiffParser
 import udp.parse.util.UnifiedDiffParserCreator
 
 import javax.ws.rs.Consumes
-import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/info")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.TEXT_PLAIN)
 @Slf4j
-public class InfoResource extends BaseResource {
-    @GET
+class InfoResource extends BaseResource {
+    @POST
     @Timed
-    public String doAction(String diffContent) {
+    Response doAction(String diffContent) {
         log.info("Info resource triggered.")
 
         UnifiedDiffParser unifiedDiffParser =
                 UnifiedDiffParserCreator.getInstance().create()
         List<UnifiedDiff> unifiedDiffs = unifiedDiffParser.parse(diffContent)
 
-        return new ObjectMapper().writerWithDefaultPrettyPrinter()
-                .writeValueAsString(unifiedDiffs)
+        return Response.ok()
+                // TODO: Is this safe?
+                .header("Access-Control-Allow-Origin", "*")
+                .entity(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(unifiedDiffs.get(0))).build()
     }
 }
